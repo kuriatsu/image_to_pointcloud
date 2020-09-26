58eb9785c3308fa543e2fab1d2b164d7a16b0a2b
kuriatsu <kuriatsubayashi712@gmail.com>
2020-09-26 18:42:23 +0900

    first commit

---
+++ src/image2pcl.cpp
@@ -0,0 +1,198 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <dynamic_reconfigure/server.h>
+#include <image2pcl/image2pclConfig.h>
+
+#include "geometry_msgs/Vector3.h"
+
+struct image_pose
+{
+    float x;
+    float y;
+    float z;
+    float roll;
+    float pitch;
+    float yaw;
+};
+
+
+class Image2Pcl
+{
+private:
+    ros::Publisher pub_image_cloud;
+
+    // ros::Timer timer;
+
+    dynamic_reconfigure::Server<image2pcl::image2pclConfig> ddynamic_server;
+    dynamic_reconfigure::Server<image2pcl::image2pclConfig>::CallbackType ddynamic_server_callback;
+
+    std::string m_image_name;
+    cv::Mat m_cv_image;
+    image_pose m_image_pose;
+    float m_image_res;
+    bool m_synchronize = false;
+
+public:
+    Image2Pcl();
+    ~Image2Pcl();
+
+private:
+    void ddynamicCb(image2pcl::image2pclConfig &config, uint32_t level);
+    void makeImageCloud();
+    // void timerCb(const ros::TimerEVent&);
+    void ddynamicUpdate();
+    void ddynamicInit();
+};
+
+
+Image2Pcl::Image2Pcl()
+{
+    ros::NodeHandle n;
+
+    ddynamic_server_callback = boost::bind(&Image2Pcl::ddynamicCb, this, _1, _2);
+    ddynamic_server.setCallback(ddynamic_server_callback);
+
+    pub_image_cloud = n.advertise<pcl::PointCloud<pcl::PointXYZRGB> >("/image_cloud", 5);
+}
+
+Image2Pcl::~Image2Pcl()
+{
+    ddynamicInit();
+}
+
+void Image2Pcl::ddynamicCb(image2pcl::image2pclConfig &config, uint32_t level)
+{
+    std::string image = config.image;
+    m_image_res = std::stof(config.image_res);
+    m_synchronize = config.synchronize;
+
+    m_image_pose.x = std::stof(config.image_x);
+    m_image_pose.y = std::stof(config.image_y);
+    m_image_pose.z = std::stof(config.image_z);
+    m_image_pose.roll = std::stof(config.image_roll) * M_PI / 180.0;
+    m_image_pose.pitch = std::stof(config.image_pitch) * M_PI / 180.0;
+    m_image_pose.yaw = std::stof(config.image_yaw) * M_PI / 180.0;
+
+
+    if (image != m_image_name)
+    {
+        m_cv_image = cv::imread(image, 1);
+        m_image_name = image;
+    }
+
+    std::cout << m_synchronize << std::endl;
+    if (m_synchronize)
+    {
+        makeImageCloud();
+    }
+}
+
+
+void Image2Pcl::ddynamicUpdate()
+{
+    image2pcl::image2pclConfig config;
+    config.image_res = std::to_string(m_image_res);
+    config.image_x = std::to_string(m_image_pose.x);
+    config.image_y = std::to_string(m_image_pose.y);
+    config.image_z = std::to_string(m_image_pose.z);
+    config.image_roll = std::to_string(m_image_pose.roll);
+    config.image_pitch = std::to_string(m_image_pose.pitch);
+    config.image_yaw = std::to_string(m_image_pose.yaw);
+    config.synchronize = m_synchronize;
+    // config.publish = false;
+
+    ddynamic_server.updateConfig(config);
+}
+
+
+void Image2Pcl::ddynamicInit()
+{
+    image2pcl::image2pclConfig config;
+    config.image_res = std::to_string(0.0);
+    config.image_x = std::to_string(0.0);
+    config.image_y = std::to_string(0.0);
+    config.image_z = std::to_string(0.0);
+    config.image_roll = std::to_string(0.0);
+    config.image_pitch = std::to_string(0.0);
+    config.image_yaw = std::to_string(0.0);
+    config.synchronize = false;
+
+    ddynamic_server.updateConfig(config);
+}
+
+void Image2Pcl::makeImageCloud()
+{
+    pcl::PointCloud<pcl::PointXYZRGB> image_cloud;
+    int height = m_cv_image.rows;
+    int width = m_cv_image.cols;
+    std::cout << width << std::endl;
+    for (int i = 0; i < height; i++)
+    {
+        cv::Vec3b *col = m_cv_image.ptr<cv::Vec3b>(i);
+        for (int j=0; j<width; j++)
+        {
+            pcl::PointXYZRGB point;
+            float x = i * m_image_res;
+            float y = j * m_image_res;
+            float z = 0.0;
+
+            float rotated_x = x * (cos(m_image_pose.yaw) * cos(m_image_pose.pitch))
+                      + y * (cos(m_image_pose.yaw) * sin(m_image_pose.pitch) * sin(m_image_pose.roll)
+                             - sin(m_image_pose.yaw) * cos(m_image_pose.roll))
+                      + z * (cos(m_image_pose.yaw) * sin(m_image_pose.pitch) * cos(m_image_pose.roll)
+                             + sin(m_image_pose.yaw) * sin(m_image_pose.roll));
+            float rotated_y = x * (sin(m_image_pose.yaw) * cos(m_image_pose.pitch))
+                      + y * (sin(m_image_pose.yaw) * sin(m_image_pose.pitch) * sin(m_image_pose.roll)
+                             + cos(m_image_pose.yaw) * cos(m_image_pose.roll))
+                      + z * (sin(m_image_pose.yaw) * sin(m_image_pose.pitch) * cos(m_image_pose.roll)
+                             - cos(m_image_pose.yaw) * sin(m_image_pose.roll));
+            float rotated_z = x * (-sin(m_image_pose.pitch))
+                      + y * (cos(m_image_pose.pitch) * sin(m_image_pose.roll))
+                      + z * (cos(m_image_pose.pitch) * cos(m_image_pose.roll));
+
+            point.x = rotated_x + m_image_pose.x;
+            point.y = rotated_y + m_image_pose.y;
+            point.z = rotated_z + m_image_pose.z;
+            point.r = col[j][2];
+            point.g = col[j][1];
+            point.b = col[j][0];
+            // std::cout << point << std::endl;
+            image_cloud.push_back(point);
+        }
+    }
+    std::cout << "pub" << std::endl;
+
+    auto msg = image_cloud.makeShared();
+    msg->header.frame_id = "map";
+    pcl_conversions::toPCL(ros::Time::now(), msg->header.stamp);
+
+    pub_image_cloud.publish(msg);
+    ddynamicUpdate();
+}
+
+//
+// void AdasVis::timerCallback(const ros::TimerEvent&)
+// {
+//     if (m_mode)
+//     {
+//         makeImageCloud();
+//     }
+// }
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "image2pcl_node");
+
+    Image2Pcl image_to_pcl;
+
+    ros::spin();
+
+    return 0;
+}
